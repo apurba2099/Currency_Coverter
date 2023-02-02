5a02445ee8ceeda8c02ea57df05dc0b290110407
Apurba Dutta <apurbadutta2099@gmail.com>
2023-02-02 19:20:16 +0530

    Added reverse convert

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-function convert(){
+function usdToInr(){
     let usd = document.getElementById('inputUsd').value;
-    let cInr=(usd * 81.78);
+    let cInr=(usd * 81.78).toFixed(2);
     document.getElementById('inputInr').value = cInr;
 }
+function inrToUsd(){
+    let inr = document.getElementById('inputInr').value;
+    let cUsd=(inr / 81.78).toFixed(2);
+    document.getElementById('inputUsd').value = cUsd;
+}
+
