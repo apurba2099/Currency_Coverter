5c834618ba7f2411bda8a69f3938c2a43b6fd558
Apurba Dutta <apurbadutta2099@gmail.com>
2025-03-23 18:32:52 +0530

    update

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-function usdToInr(){
-    let usd = document.getElementById('inputUsd').value;
-    let cInr=(usd * 81.78).toFixed(2);
-    document.getElementById('inputInr').value = cInr;
+function usdToInr() {
+  let usd = document.getElementById("inputUsd").value;
+  let cInr = (usd * 81.78).toFixed(2);
+  document.getElementById("inputInr").value = cInr;
 }
-function inrToUsd(){
-    let inr = document.getElementById('inputInr').value;
-    let cUsd=(inr / 86).toFixed(2);
-    document.getElementById('inputUsd').value = cUsd;
+function inrToUsd() {
+  let inr = document.getElementById("inputInr").value;
+  let cUsd = (inr / 86).toFixed(2);
+  document.getElementById("inputUsd").value = cUsd;
 }
-
